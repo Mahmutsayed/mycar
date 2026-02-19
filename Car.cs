fcd253909745cbd390b627185a7d672f070ec783
Gelisim University <bim@gelisim.edu.tr>
2026-02-19 16:34:30 +0300

    maho

---
+++ Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarProject
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Speed { get; set; }
+
+        //Constructor to initialize the properties
+        public Car(string brand, string color)
+        {
+            Brand = brand;
+            Color = color;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"{Brand} is starting.");
+            // Additional logic to start the car can be added here
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{Brand} is stopping.");
+            // Additional logic to stop the car can be added here
+        }
+
+        public void Accelerate(int increment)
+        {
+            // Speed = Speed + increment;
+            Speed += increment;
+            Console.WriteLine($"{Brand} is accelerating. Current speed: {Speed} km/h");
+        }
+
+        public void Decelerate(int decrement)
+        {
+            Speed -= decrement;
+            if (Speed < 0)
+                Speed = 0;
+            Console.WriteLine($"{Brand} is decelerating. Current speed: {Speed} km/h");
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace MyCarProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Toyota", "Red");
+
+            Console.WriteLine("Car Details:");
+            // Console.WriteLine($"Brand: {myCar.Brand}");
+            Console.WriteLine("Brand: " + myCar.Brand);
+            
+
+            myCar.Start();
+
+            myCar.Accelerate(20);
+            myCar.Accelerate(10);
+
+            myCar.Stop();
+
+            Console.ReadLine();
+
+
+            //Console.WriteLine("Color: " + myCar.Color);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Changed Color");
+            //myCar.Color = "Green";
+            //Console.WriteLine("Color: " + myCar.Color);
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("-------------------");
+            //Car anotherCar = new Car("Honda", "Blue");
+            //Console.WriteLine("Another Car Details:");
+            //Console.WriteLine("Brand: " + anotherCar.Brand);
+            //Console.ReadLine();
+        }
+    }
+}
